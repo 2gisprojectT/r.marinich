0734c457d3156b07427e64694ddf1a510890cd33
tyuhgce <tyuhgce@gmail.com>
2015-04-01 20:12:10 +0600

    added function of create

--- 3003_The_task_about_a_lion/main.py
+++ 3003_The_task_about_a_lion/main.py
@@ -10,3 +10,10 @@ class Signals(IntEnum):
     hunter = 1
     tree = 2
 
+def create_enum_table(init_list):
+    enum_table = []
+    for i in range(len(States)):
+        enum_table.append([])
+        for j in range(len(Signals)):
+            enum_table[i].append(init_list[i][j])
+    return enum_table
