52a4dc39bc81891260d2e56ab680063f9e9323fe
tyuhgce <tyuhgce@gmail.com>
2015-04-02 01:43:14 +0600

    Оптимизировал.

--- 3003_The_task_about_a_lion/main.py
+++ 3003_The_task_about_a_lion/main.py
@@ -1,6 +1,5 @@
 from enum import IntEnum
 import unittest
-# import numpy
 
 class States(IntEnum):
     full = 0
@@ -11,14 +10,6 @@ class Signals(IntEnum):
     hunter = 1
     tree = 2
 
-def create_enum_table(init_list):
-    enum_table = []
-    for i in range(len(States)):
-        enum_table.append([])
-        for j in range(len(Signals)):
-            enum_table[i].append(init_list[i][j])
-    return enum_table
-
 def get_singal():
     name_input = input()
     for name, member in Signals.__members__.items():
@@ -26,36 +17,41 @@ def get_singal():
             return member
     return -1
 
-def lion(c_state, c_signal):
-    init_list = [[States.hungry, States.hungry, States.hungry], [States.full, States.hungry, States.hungry]]
-    #return numpy.fromfunction(lambda i, j: init_list[i][j], (len(Signals), len(States)), dtype=IntEnum)[c_state.value][c_signal.value]
-    return create_enum_table(init_list)[c_state.value][c_signal.value]
+def lion(how_do, init_list, c_state, c_signal):
+    return how_do(c_state.value, c_signal.value, init_list)
 
 def start_lion():
     current_state = States.full
+    init_list = [[States.hungry, States.hungry, States.hungry], [States.full, States.hungry, States.hungry]]
     print("default state =", current_state.name)
     print("write next singal: antelope, hunter, tree")
     while True:
         current_signal = get_singal()
         if current_signal != -1:
-            current_state = lion(current_state, current_signal)
-            print(current_state.name)
+            current_state = lion(lambda i, j, clist: clist[i][j], init_list, current_state, current_signal)
+            print("current state =", current_state.name)
         else:
             print("try again")
 
 class TestLeon(unittest.TestCase):
     def test_FullToAntilopa(self):
-        self.assertEqual(lion(States.full, Signals.antelope), States.hungry)
+        init_list = [[States.hungry, States.hungry, States.hungry], [States.full, States.hungry, States.hungry]]
+        self.assertEqual(lion(lambda i, j, clist: clist[i][j], init_list, States.full, Signals.antelope), States.hungry)
     def test_FullToHunter(self):
-        self.assertEqual(lion(States.full, Signals.hunter), States.hungry)
+        init_list = [[States.hungry, States.hungry, States.hungry], [States.full, States.hungry, States.hungry]]
+        self.assertEqual(lion(lambda i, j, clist: clist[i][j], init_list, States.full, Signals.hunter), States.hungry)
     def test_FullToTree(self):
-        self.assertEqual(lion(States.full, Signals.tree), States.hungry)
+        init_list = [[States.hungry, States.hungry, States.hungry], [States.full, States.hungry, States.hungry]]
+        self.assertEqual(lion(lambda i, j, clist: clist[i][j], init_list, States.full, Signals.tree), States.hungry)
     def test_HungryToAntilopa(self):
-        self.assertEqual(lion(States.hungry, Signals.antelope), States.full)
-    def test_HungryToAntilopa(self):
-        self.assertEqual(lion(States.hungry, Signals.hunter), States.hungry)
-    def test_HungryToAntilopa(self):
-        self.assertEqual(lion(States.hungry, Signals.tree), States.hungry)
+        init_list = [[States.hungry, States.hungry, States.hungry], [States.full, States.hungry, States.hungry]]
+        self.assertEqual(lion(lambda i, j, clist: clist[i][j], init_list, States.hungry, Signals.antelope), States.full)
+    def test_HungryToHunter(self):
+        init_list = [[States.hungry, States.hungry, States.hungry], [States.full, States.hungry, States.hungry]]
+        self.assertEqual(lion(lambda i, j, clist: clist[i][j], init_list, States.hungry, Signals.hunter), States.hungry)
+    def test_HungryToTree(self):
+        init_list = [[States.hungry, States.hungry, States.hungry], [States.full, States.hungry, States.hungry]]
+        self.assertEqual(lion(lambda i, j, clist: clist[i][j], init_list, States.hungry, Signals.tree), States.hungry)
 
 if __name__ == '__main__':
     unittest.main()
