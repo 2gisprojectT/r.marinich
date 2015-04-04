9bf5ec48c09f5f7cbc9eb528680825d33de21ad2
tyuhgce <tyuhgce@gmail.com>
2015-04-04 17:35:50 +0600

    Исправил замечания.

--- 3003_The_task_about_a_lion/main.py
+++ 3003_The_task_about_a_lion/main.py
@@ -1,76 +1,52 @@
 class Lion():
-    def __init__(self, list_states, list_signal, list_actions, FS_indexs, FA_indexs):
-        self.states  = list_states
-        self.signals = list_signal
-        self.actions = list_actions
-        self.FS = FS_indexs
-        self.FA = FA_indexs
+
+    states =  ["full", "hungry"]
+    signals = ["antelope", "hunter", "tree"]
+    actions = ["sleep", "run", "see", "eat"]
+
+    transition_table = [["hungry", "hungry", "hungry"], ["full", "hungry", "hungry"]]
+    action_table = [["sleep", "run", "see"], ["eat", "run", "sleep"]]
+
+    state = "full"
 
     def set_init_states(self, init_states):
         self.state = init_states
 
-    def set_signal(self, signal):
-        self.signal = signal
+    def send_signal(self, signal):
+        if signal in self.signals:
+            self.signal = signal
+        else:
+            self.signal = None
 
-    def getIDSt(self, what):
+    def getID(self, what, where):
         i = 0
-        for item in self.states:
+        for item in where:
             if item == what:
                 return i
             i += 1
         return -1
-
-    def getIDSi(self, what):
-        i = 0
-        for item in self.signals:
-            if item == what:
-                return i
-            i += 1
-        return -1
-
-    def getInitState(self):
-        try: self.state
-        except: return -1
-        return self.state
-
-    def getInitSignal(self):
-        try: self.signal
-        except: return -1
-        return self.signal
 
     def getState(self):
-        try: self.state and self.signal and self.FS
-        except: return -1
-        self.state = self.FS[self.getIDSt(self.state)][self.getIDSi(self.signal)]
+        try: self.signal
+        except: return self.state
+        if self.signal is None:
+            return self.state
+        self.state = self.transition_table[self.getID(self.state, self.states)][self.getID(self.signal, self.signals)]
         return self.state
 
     def getAction(self):
-        try: self.state and self.signal and self.FA
-        except: return -1
-        return self.FA[self.getIDSt(self.state)][self.getIDSi(self.signal)]
-
+        if self.signal is not None:
+            return self.action_table[self.getID(self.state, self.states)][self.getID(self.signal, self.signals)]
+        else:
+            return "doing nothing"
 
 if __name__ == '__main__':
 
-    list_states =  ["full", "hungry"]
-    list_signals = ["antelope", "hunter", "tree"]
-    list_actions = ["sleep", "run", "see", "eat"]
+    L = Lion()
 
-    FS = [["hungry", "hungry", "hungry"], ["full", "hungry", "hungry"]]
-    FA = [["sleep", "run", "see"], ["eat", "run", "sleep"]]
-
-    L = Lion(list_states, list_signals, list_actions, FS, FA)
-    
-    L.set_init_states("full")
-
-    print("default state =", L.getInitState())
+    print("default state =", L.getState())
     print("write next singal: antelope, hunter, tree")
 
     while True:
-        current_signal = input()
-
-        if (L.getIDSi(current_signal) != -1):
-            L.set_signal(current_signal)
-            print("Lion", L.getAction(),". Current state =", L.getState() )
-        else:
-            print("try again")
+        L.send_signal(input())
+        print("Lion", L.getAction(),". Current state =", L.getState() )
--- 3003_The_task_about_a_lion/test.py
+++ 3003_The_task_about_a_lion/test.py
@@ -3,67 +3,63 @@ import main
 
 class TestLeon(unittest.TestCase):
 
-    list_states =  ["full", "hungry"]
-    list_signals = ["antelope", "hunter", "tree"]
-    list_actions = ["sleep", "run", "see", "eat"]
+    L = main.Lion()
 
-    FS = [["hungry", "hungry", "hungry"], ["full", "hungry", "hungry"]]
-    FA = [["sleep", "run", "see"], ["eat", "run", "sleep"]]
-
-    L = main.Lion(list_states, list_signals, list_actions, FS, FA)
-    L.set_init_states("full")
+    def test_CorrectInput(self):
+        self.L.send_signal("fdafg")
+        self.assertEqual( "doing nothing", self.L.getAction() )
 
     def test_FullToAntilopa(self):
         self.L.set_init_states("full")
-        self.L.set_signal("antelope")
+        self.L.send_signal("antelope")
         self.assertEqual( "hungry", self.L.getState() )
     def test_FullToHunter(self):
         self.L.set_init_states("full")
-        self.L.set_signal("hunter")
+        self.L.send_signal("hunter")
         self.assertEqual( "hungry", self.L.getState() )
     def test_FullToTree(self):
         self.L.set_init_states("full")
-        self.L.set_signal("tree")
+        self.L.send_signal("tree")
         self.assertEqual( "hungry", self.L.getState() )
     def test_HungryToAntilopa(self):
         self.L.set_init_states("hungry")
-        self.L.set_signal("antelope")
+        self.L.send_signal("antelope")
         self.assertEqual( "full", self.L.getState() )
     def test_HungryToHunter(self):
         self.L.set_init_states("hungry")
-        self.L.set_signal("hunter")
+        self.L.send_signal("hunter")
         self.assertEqual( "hungry", self.L.getState() )
     def test_HungryToTree(self):
         self.L.set_init_states("hungry")
-        self.L.set_signal("tree")
+        self.L.send_signal("tree")
         self.assertEqual( "hungry", self.L.getState() )
 
     def test_ActionSleep1(self):
         self.L.set_init_states("full")
-        self.L.set_signal("antelope")
+        self.L.send_signal("antelope")
         self.assertEqual( "sleep", self.L.getAction() )
     def test_ActionSleep2(self):
         self.L.set_init_states("hungry")
-        self.L.set_signal("tree")
+        self.L.send_signal("tree")
         self.assertEqual( "sleep", self.L.getAction() )
 
     def test_ActionRun1(self):
         self.L.set_init_states("full")
-        self.L.set_signal("hunter")
+        self.L.send_signal("hunter")
         self.assertEqual( "run", self.L.getAction() )
     def test_ActionRun2(self):
         self.L.set_init_states("hungry")
-        self.L.set_signal("hunter")
+        self.L.send_signal("hunter")
         self.assertEqual( "run", self.L.getAction() )
 
     def test_ActionSee(self):
         self.L.set_init_states("full")
-        self.L.set_signal("tree")
+        self.L.send_signal("tree")
         self.assertEqual( "see", self.L.getAction() )
 
     def test_ActionEat(self):
         self.L.set_init_states("hungry")
-        self.L.set_signal("antelope")
+        self.L.send_signal("antelope")
         self.assertEqual( "eat", self.L.getAction() )
 
 if __name__ == '__main__':
