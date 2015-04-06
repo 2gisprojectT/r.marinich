75b444617d423ecb8019cd44214dcf5c205abf98
tyuhgce <tyuhgce@gmail.com>
2015-04-06 20:15:34 +0600

    Убрал константы в тестах.

--- 3003_The_task_about_a_lion/test.py
+++ 3003_The_task_about_a_lion/test.py
@@ -18,25 +18,27 @@ class TestLeon(unittest.TestCase):
         self.transition_table = [["hungry", "hungry", "hungry"], ["full", "hungry", "hungry"]]
         self.action_table = [["sleep", "run", "see"], ["eat", "run", "sleep"]]
 
+        self.stateFull = "full"
+        self.stateHungry = "hungry"
         self.error_state = "doing nothing"
 
     def test_CorrectInputSinnal(self):
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, "full", self.error_state)
+        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
         L.send_signal("fdafg")
         self.assertEqual( "doing nothing", L.getAction() )
 
     def test_Constructor(self):
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, "full", self.error_state)
+        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
         self.assertEqual(L.states, self.states)
         self.assertEqual(L.signals, self.signals)
         self.assertEqual(L.actions, self.actions)
         self.assertEqual(L.transition_table, self.transition_table)
         self.assertEqual(L.action_table, self.action_table)
-        self.assertEqual(L.state, "full")
+        self.assertEqual(L.state, self.stateFull)
         self.assertEqual(L.error_state, self.error_state)
 
     def test_SendSignalAndGetState(self):
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, "full", self.error_state)
+        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
         L.send_signal("antelope")
         self.assertEqual( "hungry", L.getState() )
 
@@ -46,12 +48,12 @@ class TestLeon(unittest.TestCase):
         L.send_signal("tree")
         self.assertEqual( "hungry", L.getState() )
 
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, "hungry", self.error_state)
+        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateHungry, self.error_state)
         L.send_signal("antelope")
         self.assertEqual( "full", L.getState() )
 
     def test_SendSignalAndGetAction(self):
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, "full", self.error_state)
+        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
         L.send_signal("antelope")
         self.assertEqual( "sleep", L.getAction() )
 
