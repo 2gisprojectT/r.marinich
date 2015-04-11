6b3ecd6146656a8f136417c8d7f56470285ea0bb
tyuhgce <tyuhgce@gmail.com>
2015-04-11 16:10:21 +0600

    Вынес созданите льва в setUP

--- 3003_The_task_about_a_lion/test.py
+++ 3003_The_task_about_a_lion/test.py
@@ -17,37 +17,33 @@ class TestLeon(unittest.TestCase):
 
         self.transition_table = [["test_states"], ["test_states"]]
         self.action_table = [["test_action"], ["test_action"]]
-
         self.stateFull = "test"
         self.stateHungry = "test"
         self.error_state = "test"
+        self.L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
 
     def test_send_signal(self):
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
-        L.send_signal("test_signal")
-        self.assertEqual(L.signal, "test_signal")
-        L.send_signal("abc")
-        self.assertEqual(L.signal, None)
+        self.L.send_signal("test_signal")
+        self.assertEqual(self.L.signal, "test_signal")
+        self.L.send_signal("abc")
+        self.assertEqual(self.L.signal, None)
 
     def test_getState(self):
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
-        L.signal = self.signals[0]
-        self.assertEqual("test_states", L.getState())
+        self.L.signal = self.signals[0]
+        self.assertEqual("test_states", self.L.getState())
 
     def test_GetAction(self):
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
-        L.signal = self.signals[0]
-        self.assertEqual("test_action", L.getAction())
+        self.L.signal = self.signals[0]
+        self.assertEqual("test_action", self.L.getAction())
 
     def test_Constructor(self):
-        L = main.Lion(self.states, self.signals, self.actions, self.transition_table, self.action_table, self.stateFull, self.error_state)
-        self.assertEqual(L.states, self.states)
-        self.assertEqual(L.signals, self.signals)
-        self.assertEqual(L.actions, self.actions)
-        self.assertEqual(L.transition_table, self.transition_table)
-        self.assertEqual(L.action_table, self.action_table)
-        self.assertEqual(L.state, self.stateFull)
-        self.assertEqual(L.error_state, self.error_state)
+        self.assertEqual(self.L.states, self.states)
+        self.assertEqual(self.L.signals, self.signals)
+        self.assertEqual(self.L.actions, self.actions)
+        self.assertEqual(self.L.transition_table, self.transition_table)
+        self.assertEqual(self.L.action_table, self.action_table)
+        self.assertEqual(self.L.state, self.stateFull)
+        self.assertEqual(self.L.error_state, self.error_state)
 
 if __name__ == '__main__':
     unittest.main()
